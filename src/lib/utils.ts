a8abe72d656310b614dbb545cadf0787f3c67f2b
damino312 <daminionq@gmail.com>
2024-11-18 22:31:54 +0700

    FIX SHADCN bug

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,9 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function removePointerEventsFromBody() {
+  if (document.body.style.pointerEvents === "none") {
+      document.body.style.pointerEvents = "";
+  }
+}
